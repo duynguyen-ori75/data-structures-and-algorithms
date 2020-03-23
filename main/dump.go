2fc0d40a4b120a858977ddf16b748f8ad0d13f40
Duy Nguyen <duynguyen.ori75@gmail.com>
2020-03-23 15:01:13 +0700

    Random update to dump.go

--- main/dump.go
+++ main/dump.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sync"
 	"sync/atomic"
 	"time"
 )
 
-func formula(value int) int {
-	//return value
-	return value ^ value%12345678
+const MODULO int64 = 12345678
+
+func power(x int64, y int64) int64 {
+	if y <= 1 {
+		return int64(math.Pow(float64(x), float64(y)))
+	}
+	half := power(x, y / 2) % MODULO
+	half = (half * half) % MODULO
+	if y % 2 == 0 {
+		return half
+	} else {
+		return (half * x) % MODULO
+	}
 }
 
 func timeTrack(start time.Time, name string) {
@@ -17,55 +28,55 @@ func timeTrack(start time.Time, name string) {
 	fmt.Printf("%s took %s\n", name, elapsed)
 }
 
-func normalAggregation(n int) int {
+func normalAggregation(n int64) int64 {
 	defer timeTrack(time.Now(), "Normal aggregation")
-	result := 0
-	for element := 0; element <= n; element++ {
-		result += formula(element)
+	var result int64 = 0
+	for element := int64(1); element <= n; element++ {
+		result = (result + power(element, element)) % MODULO
 	}
 	return result
 }
 
-func goroutinesAggregation(n int) int {
+func goroutinesAggregation(n int64) int64 {
 	defer timeTrack(time.Now(), "Goroutines aggregation")
 	var result int64 = 0
 	var wg sync.WaitGroup
-	wg.Add(n)
-	for element := 1; element <= n; element++ {
-		go func(element int) {
-			atomic.AddInt64(&result, int64(formula(element)))
+	wg.Add(int(n))
+	for element := int64(1); element <= n; element++ {
+		go func(element int64) {
+			for {
+				oldResult := result
+				expectedResult := (oldResult + power(element, element)) % int64(MODULO)
+				if atomic.CompareAndSwapInt64(&result, oldResult, expectedResult) {
+					break
+				}
+			}
 			wg.Done()
 		}(element)
 	}
 	wg.Wait()
-	return int(result)
+	return result
 }
 
-func generatorAggregation(n int) int {
+func generatorAggregation(n int64) int64 {
 	defer timeTrack(time.Now(), "Generator aggregation")
 	numberOfWorkers := 4
 	var wg sync.WaitGroup
 	wg.Add(numberOfWorkers)
-	allAlements, summary := make(chan int), make(chan int, numberOfWorkers)
+	allAlements, summary := make(chan int64), make(chan int64, numberOfWorkers)
 	// int generator
-	go func(n int) {
-		for element := 1; element <= n; element++ {
+	go func(n int64) {
+		for element := int64(1); element <= n; element++ {
 			allAlements <- element
 		}
-		for idx := 0; idx < numberOfWorkers; idx++ {
-			allAlements <- -1
-		}
+		close(allAlements)
 	}(n)
 	// 4 goroutines receiving int and doing aggregation
 	for numRoutine := 0; numRoutine < numberOfWorkers; numRoutine++ {
 		go func() {
-			tmpResult := 0
-			for {
-				nextElement := <-allAlements
-				if nextElement < 0 {
-					break
-				}
-				tmpResult += formula(nextElement)
+			var tmpResult int64 = 0
+			for nextElement := range allAlements {
+				tmpResult = (tmpResult + power(nextElement, nextElement)) % MODULO
 			}
 			summary <- tmpResult
 			wg.Done()
@@ -73,15 +84,15 @@ func generatorAggregation(n int) int {
 	}
 	wg.Wait()
 	// aggregate all values
-	result := 0
+	var result int64 = 0
 	for numRoutine := 0; numRoutine < numberOfWorkers; numRoutine++ {
-		result += <-summary
+		result = (result + <-summary) % MODULO
 	}
 	return result
 }
 
 func main() {
-	fmt.Println(normalAggregation(1000000))
-	fmt.Println(goroutinesAggregation(1000000))
-	fmt.Println(generatorAggregation(1000000))
+	fmt.Println(normalAggregation(10000))
+	fmt.Println(goroutinesAggregation(10000))
+	fmt.Println(generatorAggregation(10000))
 }
